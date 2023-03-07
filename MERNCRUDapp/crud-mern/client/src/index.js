2349113ab256bace5329e82fe32c8706264e46ca
Angry Boy <suvxdas@gmail.com>
2023-03-07 18:42:04 +0530

    new updates

--- MERNCRUDapp/crud-mern/client/src/index.js
+++ MERNCRUDapp/crud-mern/client/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import CreatePost from "./CreatePost"
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/create" element={<CreatePost />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
