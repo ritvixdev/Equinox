f5b73dcac9cd8e2eb7c391edd2206ea0d2740142
Angry Boy <suvxdas@gmail.com>
2023-03-09 23:25:57 +0530

    updates

---
+++ MERNCRUDapp/crud-mern/client/src/CreatePost.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Button, Form, FormControl, FormGroup } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const CreatePost = () => {
+  const navigate = useNavigate();
+  const [post, setPost] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault(); // So, it dosen't reload the page
+
+    // api call
+    //axios.post("http://localhost:3001/create") we will create proxy in package.json to avoid this
+    axios
+      .post("/create", post)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  //   useEffect (() => {
+  //     console.log(post);
+  //   }, [post])
+
+  return (
+    <div style={{ width: "90%", margin: "auto auto", textAlign: "center" }}>
+      <h1>Create Post</h1>
+      <Form>
+        <FormGroup>
+          <FormControl
+            name="title"
+            value={post.title}
+            placeholder="Title"
+            style={{ marginBottom: "1rem" }}
+            onChange={handleChange}
+          />
+          <FormControl
+            name="description"
+            value={post.description}
+            placeholder="Descriptionm"
+            style={{ marginBottom: "1rem" }}
+            onChange={handleChange}
+          />
+        </FormGroup>
+        <Button
+          variant="outline-success"
+          style={{ width: "100%", marginBottom: "1rem" }}
+          onClick={handleClick}
+        >
+          Create Post
+        </Button>
+      </Form>
+      <Button
+        variant="outline-dark"
+        style={{ width: "100%" }}
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </Button>
+    </div>
+  );
+};
+
+export default CreatePost;
--- MERNCRUDapp/crud-mern/server/server.js
+++ MERNCRUDapp/crud-mern/server/server.js
@@ -1,11 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+
+// creating middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("express is here");
+});
+
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.hrp39hb.mongodb.net/?retryWrites=true&w=majority`).catch((err) => console.log(err))
+
+//DB schema and model
+
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String
+})
+
+const Post = mongoose.model("Post", postSchema)
+
+app.post("/create", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    description: req.body.description,
+  }).then(doc => console.log(doc)).catch(err => console.log(err))
 });
 
 app.listen(3001, () => {
